f0022646ed02d9df12bb5d276f4807363459429c
pushparajcs2002 <pushparajcs2002@gmail.com>
2024-03-27 19:56:12 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import SubHeading from './SubHeading';
+import Newsletter from './Newsletter';
+import FooterOverlay from './FooterOverlay';
+import MenuItem from './MenuItem';
+import Navbar from './Navbar';
+
+export {
+  SubHeading,
+  Newsletter,
+  FooterOverlay,
+  MenuItem,
+  Navbar,
+};
